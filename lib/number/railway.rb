4d097660aa82daa429d3facc6016659f0d20f8de
skytomo <tomo1kawa.fullerene@gmail.com>
2024-04-05 16:14:07 +0000

    feat: git init

---
+++ lib/number/railway.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "railway/version"
+
+module Number
+  module Railway
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
